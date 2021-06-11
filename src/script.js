ee0ffce74a3ef75834f1e44480a76a2ae48ef88e
Pixmew <universeyash4@gmail.com>
2021-06-11 11:53:52 +0530

    added a three js project with dragControls and normal caera controls

---
+++ src/script.js
@@ -0,0 +1,147 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
+import * as dat from 'dat.gui'
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+var obj = [];
+const geometry = new THREE.BoxGeometry(1,1,1,1);
+
+// Materials
+
+const material = new THREE.MeshBasicMaterial()
+material.color = new THREE.Color(0xff0000)
+
+// Mesh
+for(var i = 0 ; i < 100 ; i++)
+{
+    const sphere = new THREE.Mesh(geometry,new THREE.MeshLambertMaterial({color:Math.random()*0xffffff}))
+    sphere.position.x = Math.random() * 10 - 5;
+    sphere.position.y = Math.random() * 10 - 5;
+    sphere.position.z = Math.random() * 10 - 5;
+    scene.add(sphere)
+    obj.push(sphere);
+}
+
+
+// Lights
+for(var i = 0 ; i < 4 ; i ++){
+    const pointLight = new THREE.PointLight(0xffffff, 1 , 100)
+    pointLight.position.x = Math.random() * 20 - 10;
+    pointLight.position.y = Math.random() * 20 - 10;
+    pointLight.position.z = Math.random() * 20 - 10;
+    scene.add(pointLight)
+}
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+var DragControl = new DragControls(obj ,camera , renderer.domElement);
+
+// add event listener to highlight dragged objects
+
+DragControl.addEventListener( 'dragstart', function ( event ) {
+
+	controls.enabled = false;
+
+} );
+
+DragControl.addEventListener( 'dragend', function ( event ) {
+
+    controls.enabled = true;
+	
+} );
+
+
+
+//Helper
+const size = 10;
+const divisions = 10;
+
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+
+
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    //sphere.rotation.y = .5 * elapsedTime
+
+    // Update Orbital Controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
